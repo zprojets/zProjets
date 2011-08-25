6d4c58c8812591ac2293278c206f4b871f13cf19
the_little <leo84.martinez@gmail.com>
2011-08-25 11:17:18 +0200

    add EWZRecaptchaBundle as a vendor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new zProjets\UserBundle\zProjetsUserBundle(),
+            new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- app/autoload.php
+++ app/autoload.php
@@ -16,6 +16,7 @@ $loader->registerNamespaces(array(
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'FOS' => __DIR__.'/../vendor/bundles',
     'zProjets'          => __DIR__.'/../src',
+    'EWZ'               => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
