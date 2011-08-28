ba40a7c298c1b93711560b8a702906f42461fb20
the_little <leo84.martinez@gmail.com>
2011-08-28 13:18:42 +0200

    disable class cache and bootstrap for dev environment

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,11 +10,13 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+//require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
